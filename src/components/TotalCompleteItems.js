85d59223661dd25edbcce2486120581e3e6d6a8e
Dmitrena <dimas2005kv@gmail.com>
2022-04-19 14:20:35 +0300

    second commit

--- src/components/TotalCompleteItems.js
+++ src/components/TotalCompleteItems.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
+import {Counter} from "./styles/InputStyles";
 
 const TotalCompleteItems = () => {
     const completedTodos = useSelector((state) => state.todos.filter((todo) =>todo.completed ===true))
-    return <h4 className='mt-3'>Todos({completedTodos.length})</h4>;
+    return <Counter>Todos({completedTodos.length})</Counter>;
 };
 
 export default TotalCompleteItems;
