403bbabdb1724be56243e4fbbd12143f73938996
Dmitrena <dimas2005kv@gmail.com>
2022-04-19 13:18:34 +0300

    without styles

---
+++ src/globalStyles.js
@@ -0,0 +1,17 @@
+import styled ,{createGlobalStyle} from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: lightblue !important;
+  }
+
+  
+`;
